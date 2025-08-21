f610f6e46118ef9ad36ea1d3bf267529e9bb0a1f
RERUS21 <giuseppe.russotto@live.it>
2025-08-21 19:46:02 +0200

    Update train.py

--- NLQ/2D-TAN/moment_localization/train.py
+++ NLQ/2D-TAN/moment_localization/train.py
@@ -1,6 +1,6 @@
-from _future_ import absolute_import
-from _future_ import division
-from _future_ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import _init_paths
 import os
